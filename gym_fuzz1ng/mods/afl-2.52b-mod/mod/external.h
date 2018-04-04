dcbb29a9dc0ecb42d89b721d0a14cc39ce93c0dc
Stanislas Polu <polu.stanislas@gmail.com>
2018-04-04 18:27:31 +0200

    wip afl_forkserver

--- gym_fuzz1ng/mods/afl-2.52b-mod/mod/external.h
+++ gym_fuzz1ng/mods/afl-2.52b-mod/mod/external.h
@@ -8,46 +8,33 @@
 #include "types.h"
 #include "config.h"
 
-// WARNING: sync this with python models
 #define MAX_INPUT_SIZE  (1 << 12)
 
-#define MAX_EXTERNAL_CLIENTS	64
-
-#define EXTERNAL_INPUT_QUEUE_NAME "/afl-external-input"
-#define EXTERNAL_CLIENT_QUEUE_NAME "/afl-external-client-%08d"
-#define EXTERNAL_CLIENT_SHM_NAME "/afl-external-client-%08d"
+#define SEM_PING_SIGNAL_NAME "/afl-ping-signal"
+#define SEM_PONG_SIGNAL_NAME "/afl-pong-signal"
+#define SHARED_MEM_NAME "/afl-shared-mem"
 
 #pragma pack(1)
 typedef struct ping_msg_hdr {
     u32 msgid;
-    u32	type;
     u32 inputsize;
 } PING_MSG_HDR;
 
-typedef struct pong_msg_hdr {
+typedef struct pong_msg {
     u32 msgid;
     u32 status;
     u32 size;   // always MAP_SIZE?
-} PONG_MSG_HDR;
-
-
+    u8  trace_bits[MAP_SIZE];
+} PONG_MSG;
 
 #pragma pack()
 
-
 #define SHM_MAX_SIZE    	(int)MAX(MAP_SIZE, MAX_INPUT_SIZE)
 #define MESSAGE_MAX_SIZE	(int)MAX(sizeof (PING_MSG_HDR), sizeof (PONG_MSG_HDR))
-
-// defines for ping_msg_hdr->type
-#define PING_NORMAL_MESSAGE 		0
-#define PING_NEW_CLIENT_MESSAGE 	1
-#define PING_DELETE_CLIENT_MESSAGE 	2
-
 
 // defines for pong_msg_hdr->status
 #define STATUS_CRASHED	0x80000000
 #define STATUS_HANGED	0x40000000
 #define STATUS_OK		0
-
 
 #endif
--- test/dummy_simple_bits.py
+++ test/dummy_simple_bits.py
@@ -15,12 +15,14 @@ def main():
         1, 1, 255,
         1, 1, 255,
         12, 7, 5, 255,
+        1, 1, 255,
+        1, 1, 255,
     ]
     for i in inputs:
         obs, reward, done, info = env.step(i)
         c.add(info['step_coverage'])
 
-        print("STEP: reward={} done={} step={}/{} current={}/{} total={}/{} sum={}/{} action={}".format(
+        print("STEP: reward={} done={} step={}/{} current={}/{} total={}/{} sum={}/{} action={}\n".format(
             reward, done,
             info['step_coverage'].path_count(),
             info['step_coverage'].transition_count(),
