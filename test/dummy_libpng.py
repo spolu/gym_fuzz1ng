b48a5654a4ebde68620aa215f1f1ce1bbfb31338
Stanislas Polu <polu.stanislas@gmail.com>
2018-04-05 12:45:23 +0200

    bump version to 0.0.6

--- test/dummy_libpng.py
+++ test/dummy_libpng.py
@@ -19,14 +19,26 @@ def main():
     for i in inputs:
         obs, reward, done, info = env.step(i)
         c.add(info['step_coverage'])
-        print("STEP {}: {} {}".format(i, reward, done))
-        if i == 282:
-            print("COUNTS {}/{} {}/{}".format(
-                info['step_coverage'].path_count(),
-                info['step_coverage'].transition_count(),
-                c.path_count(),
-                c.transition_count(),
-            ))
+
+        print("STEP: reward={} done={} step={}/{}/{} current={}/{}/{} total={}/{}/{} sum={}/{}/{} action={}".format(
+            reward, done,
+            info['step_coverage'].path_count(),
+            info['step_coverage'].transition_count(),
+            info['step_coverage'].crash_count(),
+            info['current_coverage'].path_count(),
+            info['current_coverage'].transition_count(),
+            info['current_coverage'].crash_count(),
+            info['total_coverage'].path_count(),
+            info['total_coverage'].transition_count(),
+            info['total_coverage'].crash_count(),
+            c.path_count(),
+            c.transition_count(),
+            c.crash_count(),
+            i,
+        ))
+        if done:
+            env.reset()
+            print ("DONE!")
 
 if __name__ == "__main__":
     main()
