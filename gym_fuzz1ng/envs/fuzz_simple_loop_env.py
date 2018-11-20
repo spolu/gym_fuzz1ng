1c8cac88c9b3cb9e5f9cb5396647a81af5a96eaf
Stanislas Polu <polu.stanislas@gmail.com>
2018-11-20 20:21:05 +0000

    add mod and env file

---
+++ gym_fuzz1ng/envs/fuzz_simple_loop_env.py
@@ -0,0 +1,27 @@
+import gym_fuzz1ng
+import gym_fuzz1ng.coverage as coverage
+
+from gym_fuzz1ng.envs.fuzz_token_base_env import FuzzTokenBaseEnv
+from gym_fuzz1ng.envs.fuzz_word_base_env import FuzzWordBaseEnv
+
+
+class FuzzTokenSimpleLoopEnv(FuzzTokenBaseEnv):
+    def __init__(self):
+        self.max_input_size = 16
+        self.target_path = gym_fuzz1ng.simple_loop_target_path()
+        self.dict = coverage.Dictionary({
+            'tokens': [],
+            'bytes': True,
+        })
+        super(FuzzTokenSimpleLoopEnv, self).__init__()
+
+
+class FuzzWordSimpleLoopEnv(FuzzWordBaseEnv):
+    def __init__(self):
+        self.max_input_size = 16
+        self.target_path = gym_fuzz1ng.simple_loop_target_path()
+        self.dict = coverage.Dictionary({
+            'tokens': [],
+            'bytes': True,
+        })
+        super(FuzzWordSimpleLoopEnv, self).__init__()
