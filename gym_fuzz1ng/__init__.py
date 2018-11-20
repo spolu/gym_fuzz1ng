86825c52f3bda39daa93feada92afa02ea268d11
Stanislas Polu <polu.stanislas@gmail.com>
2018-11-20 14:55:40 +0000

    flake8 pass to init

--- gym_fuzz1ng/__init__.py
+++ gym_fuzz1ng/__init__.py
@@ -2,13 +2,21 @@ import os
 
 from gym.envs.registration import register
 
+
 def afl_forkserver_path():
     package_directory = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(package_directory, 'mods/afl-2.52b-mod/afl-2.52b/afl-forkserver')
+    return os.path.join(
+        package_directory, 'mods/afl-2.52b-mod/afl-2.52b/afl-forkserver',
+    )
+
 
 def libpng_target_path():
     package_directory = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(package_directory, 'mods/libpng-1.6.34-mod/libpng_simple_fopen_afl')
+    return os.path.join(
+        package_directory, 'mods/libpng-1.6.34-mod/libpng_simple_fopen_afl',
+    )
+
+
 register(
     id='FuzzTokenLibPNG-v0',
     entry_point='gym_fuzz1ng.envs:FuzzTokenLibPNGEnv',
@@ -18,9 +26,14 @@ register(
     entry_point='gym_fuzz1ng.envs:FuzzWordLibPNGEnv',
 )
 
+
 def simple_bits_target_path():
     package_directory = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(package_directory, 'mods/simple_bits-mod/simple_bits_afl')
+    return os.path.join(
+        package_directory, 'mods/simple_bits-mod/simple_bits_afl',
+    )
+
+
 register(
     id='FuzzTokenSimpleBits-v0',
     entry_point='gym_fuzz1ng.envs:FuzzTokenSimpleBitsEnv',
